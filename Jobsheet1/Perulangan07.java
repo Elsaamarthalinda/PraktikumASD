fb7a43e53465e704527fd8105c6c146c06c43805
Elsaamarthalinda <elsa29389@gmail.com>
2026-02-10 10:48:04 +0700

    Perulangan

---
+++ Jobsheet1/Perulangan07.java
@@ -0,0 +1,39 @@
+package Jobsheet1;
+import java.util.Scanner;
+public class Perulangan07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String NIM;
+        int n;
+
+        System.out.print("Masukkan NIM: ");
+        NIM = sc.nextLine();
+
+        n = Integer.parseInt(NIM.substring(NIM.length() - 2));
+
+        if (n < 10) {
+            n += 10;
+        }
+
+        System.out.println("n = " + n);
+        System.out.print("Output: ");
+
+        for (int i = 1; i <=n; i++) {
+            if (i == 10 || i == 15) {
+                continue;
+            }
+
+            if (i % 3 == 0) {
+                System.out.print("# ");
+            }
+            else if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+            else {
+                System.out.print("* ");
+            }
+        }
+        sc.close();
+    }
+}

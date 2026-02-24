f59999582171c426c5cbe8404c03074cb60b5b4f
Elsaamarthalinda <elsa29389@gmail.com>
2026-02-24 08:25:52 +0700

    Percobaan 2

---
+++ Jobsheet2/MahasiswaMain7.java
@@ -0,0 +1,54 @@
+package Jobsheet2;
+
+public class MahasiswaMain7 {
+    public static void main(String[] args) {
+        
+        Mahasiswa mhs1 = new Mahasiswa();
+        mhs1.nama = "Muhammad Ali Farhan";
+        mhs1.nim = "2241720171";
+        mhs1.kelas = "SI 2J";
+        mhs1.ipk = 3.55;
+
+        mhs1.tampilkanInformasi();
+        mhs1.ubahKelas("SI 2K");
+        mhs1.updateIpk(3.60);
+        mhs1.tampilkanInformasi();
+
+        Mahasiswa mhs2 = new Mahasiswa("Elsa Marthalinda", "254107020204", 3.53, "TI 1G");
+        mhs2.tampilkanInformasi();
+    }
+}
+
+class Mahasiswa {
+    String nama;
+    String nim;
+    String kelas;
+    double ipk;
+
+    Mahasiswa() {
+
+    }
+
+    Mahasiswa(String nama, String nim, double ipk, String kelas) {
+        this.nama = nama;
+        this.nim = nim;
+        this.ipk = ipk;
+        this.kelas = kelas;
+    }
+
+    void tampilkanInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("IPK: " + ipk);
+        System.out.println("Kelas: " + kelas);
+        System.out.println("------------------------");
+    }
+
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+
+    void updateIpk(double ipkBaru) {
+        ipk = ipkBaru;
+    }
+}

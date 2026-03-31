81f14de7f192b099b3c48a3192663c33b70ff70d
Elsaamarthalinda <elsa29389@gmail.com>
2026-03-31 09:56:57 +0700

    Praktikum 4

--- Praktikum05/MahasiswaDemo07.java
+++ Praktikum05/MahasiswaDemo07.java
@@ -34,5 +34,10 @@ public class MahasiswaDemo07 {
         System.out.println("Data yang sudah terurut menggunakan SELECTION SORT (ASC): ");
         list.selectionSort();
         list.tampil();
+
+        // tambahan method insertionSort()
+        System.out.println("Data yang sudah terurut menggunakan INSERTION SORT (ASC): ");
+        list.insertionSort();
+        list.tampil();
     }
 }
